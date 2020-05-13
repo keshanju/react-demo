78c231a2074963fe1bfce6d526abfc1f2b862a19
Wings/柯善炬 <wings.ke@newtype.io>
2020-05-13 19:19:58 +0800

    11

---
+++ src/views/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginContainer from './Login'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoginContainer></LoginContainer>
+      </header>
+    </div>
+  );
+}
+
+export default App;
